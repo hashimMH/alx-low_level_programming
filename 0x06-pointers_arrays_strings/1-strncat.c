81d442dd4660fe39e85700cd1bceedffdf7a5c95
hashimtop <hashimstar27@yahoo.com>
2023-09-20 22:38:09 +0400

    push

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -5,7 +5,6 @@
  * @dest: destination string
  * @src: source string
  * @n: number of bytes of str to concatenate
- *
  * Return: a pointer to the resulting string dest
  */
 char *_strncat(char *dest, char *src, int n)
