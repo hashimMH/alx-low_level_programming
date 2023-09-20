24798dfa048bd212e2fbe9ea6dd0c1cd5dcd6b9e
hashimtop <hashimstar27@yahoo.com>
2023-09-20 22:09:01 +0400

    check0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -13,7 +13,6 @@ char *_strcat(char *dest, char *src)
 
     i = 0;
     j = 0;
-
     while (dest[i] != '\0')
         i++;
 
@@ -23,8 +22,6 @@ char *_strcat(char *dest, char *src)
         j++;
         i++;
     }
-
     dest[i] = '\0';
-
     return (dest);
 }
