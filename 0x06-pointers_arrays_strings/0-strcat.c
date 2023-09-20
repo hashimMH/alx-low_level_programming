afa1958ec3fd7b33d75d4064c839f3f5a003a9d6
hashimtop <hashimstar27@yahoo.com>
2023-09-20 22:25:47 +0400

    update

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * _strcat - concatenates two strings
  * @dest: string to append to
@@ -12,6 +13,7 @@ char *_strcat(char *dest, char *src)
 
     i = 0;
     j = 0;
+
     while (dest[i] != '\0')
         i++;
 
@@ -21,6 +23,8 @@ char *_strcat(char *dest, char *src)
         j++;
         i++;
     }
+
     dest[i] = '\0';
+
     return (dest);
 }
