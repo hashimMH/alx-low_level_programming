600b9973e5fd0104276309fa3b1ec06373940904
hashimtop <hashimstar27@yahoo.com>
2023-10-12 18:09:46 +0400

    more loops

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+void print_numbers(void);
+void print_most_numbers(void);
+int _putchar(char);
+int _isupper(int);
+int _isdigit(int);
+int mul(int, int);
+
+#endif
