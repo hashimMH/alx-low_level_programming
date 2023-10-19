f6fc8751054085fa6fb725f4d2f69bd300cc5085
hashimtop <hashimstar27@yahoo.com>
2023-10-19 13:49:57 +0400

    put

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_number - prints a number
+ * @n: Input number
+ */
+
+void print_number(int n)
+{
+	char a;
+
+	if (n == -2147483648)
+	{
+		print_number(n / 10);
+		_putchar('8');
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		print_number(n / (-1));
+	}
+	else
+	{
+		if (n >= 10)
+			print_number(n / 10);
+		a = n % 10 + '0';
+		_putchar(a);
+	}
+}
