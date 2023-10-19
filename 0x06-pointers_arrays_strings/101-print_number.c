af07e5daa882bcb835fdd18bca6b066da1a83dee
hashimtop <hashimstar27@yahoo.com>
2023-10-19 13:55:17 +0400

    puuut

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -7,29 +7,25 @@
 
 void print_number(int n)
 {
-	int res, temp, expo;
+	char a;
 
-	expo = 1;
-	/*Check negatives*/
-	if (n >= 0)
-		res = n * -1;
-	else
+	if (n == -2147483648)
 	{
-		res = n;
-		_putchar('-');
+		print_number(n / 10);
+		_putchar('8');
 	}
-
-	/*Initialize exponent variable*/
-	temp = res;
-	while (temp <= -10)
+	/*Check negatives*/
+	else if (n < 0)
 	{
-		expo *= 10;
-		temp /= 10;
+		_putchar('-');
+		print_number(n / (-1));
 	}
 	/*Main */
-	while (expo >= 1)
+	else
 	{
-		_putchar(((res / expo) % 10) * -1 + '0');
-		expo /= 10;
+		if (n >= 10)
+			print_number(n / 10);
+		a = n % 10 + '0';
+		_putchar(a);
 	}
 }
